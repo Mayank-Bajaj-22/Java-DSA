659cb1107c05e1a26128f8423789954d9a3f1a55
Mayank Bajaj <mayankbajaj672@gmail.com>
2026-01-16 11:00:35 +0530

    boiler plate and output

---
+++ 1. Variables & Data Types/BoilerPlate.java
@@ -0,0 +1,7 @@
+//! Boiler PLate code in Java
+
+public class BoilerPlate {
+    public static void main(String args[]) {
+        // code
+    }
+}
---
+++ 1. Variables & Data Types/Output.java
@@ -0,0 +1,7 @@
+public class Output {
+    public static void main(String args[]) {
+        System.out.println("Hello Mayank."); // ln ek line ka space
+        System.out.print("1 2 3 4 5\n");
+        System.out.println("* * * *\n* * *\n* *\n*");
+    }
+}
